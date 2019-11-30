39461941779f0a2ca28307955d758a87ca28fef4
bipin21 <bipinkarki172@gmail.com>
2019-11-30 20:37:10 +0545

    Add middleware

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,21 +22,18 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/posts', 'PostController@all_post')->name('all_post');
 
 // Route::get('{anypath}','HomeController@index')->where('path','.*');
+Route::group(['middleware' => 'auth'], function () {
 //category
-Route::post('/add-category','CategoryController@add_category');
-Route::get('category','CategoryController@all_category');
-Route::get('category/{id}','CategoryController@delete_category');
-Route::get('editcategory/{id}','CategoryController@edit_category');
-Route::post('update-category/{id}','CategoryController@update_category');
+    Route::post('/add-category', 'CategoryController@add_category');
+    Route::get('category', 'CategoryController@all_category');
+    Route::get('category/{id}', 'CategoryController@delete_category');
+    Route::get('editcategory/{id}', 'CategoryController@edit_category');
+    Route::post('update-category/{id}', 'CategoryController@update_category');
 
 //post
-Route::post('/savepost','PostController@add_post');
-Route::get('post','PostController@all_post');
-Route::get('delete/{id}','PostController@delete_post');
-Route::get('post/{id}','PostController@edit_post');
-Route::post('update/{id}','PostController@update_post');
-
-
-
-
-
+    Route::post('/savepost', 'PostController@add_post');
+    Route::get('post', 'PostController@all_post');
+    Route::get('delete/{id}', 'PostController@delete_post');
+    Route::get('post/{id}', 'PostController@edit_post');
+    Route::post('update/{id}', 'PostController@update_post');
+});
